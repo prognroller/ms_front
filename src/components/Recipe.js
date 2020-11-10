b78a8a098e8b042e80ccff72acf5f357b8e55753
prognroller <shvetsov1988@gmail.com>
2020-11-10 23:48:19 +0200

    Refactoring

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,14 +2,14 @@ import React from "react";
 import Image from "./Image";
 import {Link} from "react-router-dom";
 
-export default function Recipe({id, name, photo}) {
+export default function Recipe({id, title, photo}) {
   return(
     <article className="recipe" >
-      {name}
       {
         photo &&
         <Link to={`/recipes/${id}`} >
           <Image image={photo} />
+          <h3>{title}</h3>
         </Link>
       }
     </article>
